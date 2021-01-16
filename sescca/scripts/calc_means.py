121ae10e4dc19363df28f1af9c57444c875fdbda
Juan Sebastián Bravo <jbravomeneses@gmail.com>
2021-01-15 22:24:21 -0500

    Scripts para funciones externas

---
+++ sescca/scripts/calc_means.py
@@ -0,0 +1,50 @@
+import threading
+from dashboard.models import Student, Campus, Worktime, Section, Group
+
+def calculate():
+    students = Student.objects.all()
+    campusses = Campus.objects.all()
+    worktimes = Worktime.objects.all()
+    sections = Section.objects.all()
+    groups = Group.objects.all()
+    if campusses:
+        for campus in campusses:
+            mean = 0
+            students_in = 0
+            for student in students:
+                if campus == student.campus:
+                    students_in += 1
+                    mean = mean + student.score
+            campus.mean_score = mean / students_in
+            campus.save()
+    if worktimes:
+        for worktime in worktimes:
+            mean = 0
+            students_in = 0
+            for student in students:
+                if worktime == student.worktime:
+                    students_in += 1
+                    mean = mean + student.score
+            worktime.mean_score = mean / students_in
+            worktime.save()
+    if sections:
+        for section in sections:
+            mean = 0
+            students_in = 0
+            for student in students:
+                if section == student.section:
+                    students_in += 1
+                    mean = mean + student.score
+            section.mean_score = mean / students_in
+            section.save()
+    if groups:
+        for group in groups:
+            mean = 0
+            for student in group.students.all():
+                mean = mean + student.score
+            group.mean_score = mean / len(group.students.all())
+            group.save()
+    run()
+def run():
+    clock = threading.Timer(10, calculate)
+    clock.start()
---
+++ sescca/scripts/time_interrupt.py
@@ -0,0 +1,40 @@
+import threading, time, requests, random, os
+from dashboard.models import AutoEvaluation
+from dashboard.models import Student
+payload = {'data':'alert'}
+
+def timer_interrupt():
+    print("Interrupt")
+    students = Student.objects.all()
+    for student in students:
+        if student.ip_board:
+            response = os.popen(f"ping -c 4 {student.ip_board}").read()
+            if "4 received" in response:
+                url = "http://" + student.ip_board
+                r = requests.get(url, params=payload)
+                print(r.status_code)
+            else:
+                print("No hay conexión")
+    run()
+
+def run():
+    print("Start")
+    timer = AutoEvaluation.objects.get(id=1)
+    if timer.activate is True:
+        timer_b = timer.time_range * 60 + timer.time_range * 30
+        timer_a = 45
+        last_time = timer.time_range
+        clock = threading.Timer(random.randint(timer_a, timer_b), timer_interrupt)
+        clock.start()
+        while timer.activate is True:
+            time.sleep(0.5)
+            timer = AutoEvaluation.objects.get(id=1)
+            if timer.time_range != last_time:
+                print("break")
+                break
+        clock.cancel()
+        run()
+    else:
+        pass
+        time.sleep(1)
+        run()
