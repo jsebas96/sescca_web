6c4fa0ccb1b21ea1efa6f9440290920f606ed119
Juan Sebastián Bravo <jbravomeneses@gmail.com>
2021-01-27 12:04:12 -0500

    Filtrado para envio de señal a a tarjetas segun la seccion de la vista activa

--- sescca/scripts/time_interrupt.py
+++ sescca/scripts/time_interrupt.py
@@ -1,20 +1,26 @@
 import threading, time, requests, random, os
 from evaluation.models import AutoEvaluation
 from school.models import Student
+from core.models import InterfaceView
 payload = {'data':'alert'}
 
 def timer_interrupt():
     print("Interrupt")
-    students = Student.objects.all()
-    for student in students:
-        if student.ip_board:
-            response = os.popen(f"ping -c 4 {student.ip_board}").read()
-            if "4 received" in response:
-                url = "http://" + student.ip_board
-                r = requests.get(url, params=payload)
-                print(r.status_code)
-            else:
-                print("No hay conexión")
+    views = InterfaceView.objects.all()
+    for view in views:
+        if view.active == True:
+            section = view.section
+    if section:
+        students = Student.objects.filter(section=section)
+        for student in students:
+            if student.ip_board:
+                response = os.popen(f"ping -c 4 {student.ip_board}").read()
+                if "4 received" in response:
+                    url = "http://" + student.ip_board
+                    r = requests.get(url, params=payload)
+                    print(r.status_code)
+                else:
+                    print("No hay conexión")
     run()
 
 def run():
@@ -24,7 +30,7 @@ def run():
         timer_b = timer.time_range * 60 + timer.time_range * 30
         timer_a = 45
         last_time = timer.time_range
-        clock = threading.Timer(random.randint(timer_a, timer_b), timer_interrupt)
+        clock = threading.Timer(10, timer_interrupt)
         clock.start()
         while timer.activate is True:
             time.sleep(0.5)
