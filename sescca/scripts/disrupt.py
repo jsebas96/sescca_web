34ae03190a8885fa49dd9b0ecb793a56127586d2
jsebas96 <jbravomeneses@gmail.com>
2021-02-09 14:35:07 -0500

    Disminucion de puntaje cada 60 segundos y actualizacion solo cuando el puntaje sea mayor a cero

--- sescca/scripts/disrupt.py
+++ sescca/scripts/disrupt.py
@@ -2,6 +2,7 @@ import threading, os, requests
 from school.models import Student
 from core.models import InterfaceView
 from evaluation.models import Disruption
+from time import sleep
 salida = False
 
 def disruption():
@@ -19,8 +20,9 @@ def disruption():
                         url = "http://" + student.ip_board
                         r = requests.get(url, params={'minus':'true'})
                         print(r.status_code)
-                        student.score = student.score - 1
-                        student.accum_score = student.accum_score - 1
+                        if student.score > 0:
+                            student.score = student.score - 1
+                            student.accum_score = student.accum_score - 1
                         student.disruption = False
                         student.save()
 
@@ -32,9 +34,10 @@ def disruption():
 def run():
     global salida
     print("Start")
+    sleep(1)
     disrupt = Disruption.objects.get(id=1)
     if disrupt.active is True:
-        clock = threading.Timer(10, disruption)
+        clock = threading.Timer(60, disruption)
         clock.start()
         while disrupt.active is True:
             disrupt = Disruption.objects.get(id=1)
